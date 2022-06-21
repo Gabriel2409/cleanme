c8719a8864e92a3521c8af1deebf33edd8cda39f
Gabriel2409 <gabriel.cournelle@gmail.com>
2022-06-21 21:12:23 +0200

    Initial commit

---
+++ gilded_rose/test_gilded_rose.py
@@ -0,0 +1,6 @@
+from gilded_rose.main import Item, GildedRose
+def test_foo():
+    items = [Item("foo", 0, 0)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].name == "fixme"
