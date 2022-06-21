aae13525143ecdbaa253348404e30f96eba6ca81
Gabriel2409 <gabriel.cournelle@gmail.com>
2022-06-21 21:33:16 +0200

    ran precommit on files

--- gilded_rose/main.py
+++ gilded_rose/main.py
@@ -1,24 +1,34 @@
 # inspired by https://github.com/emilybache/GildedRose-Refactoring-Kata and simplified
 
+
 class Item:
+    """Item to be used by Gilded Rose"""
+
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
 
     def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+        return f"{self.name}, {self.sell_in}, {self.quality}"
 
-class GildedRose(object):
+
+class GildedRose:
+    """Main Inventory. update_quality runs once a day"""
+
     def __init__(self, items=[]):
         self.items = items
 
     def update_quality(self):
+        """updates the quality of all the Items at the end of the day"""
         for item in self.items:
-            if  item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality>0 :
-                    if item.name   != "Sulfuras, Hand of Ragnaros":
-                        item.quality    = item.quality - 1
+            if (
+                item.name != "Aged Brie"
+                and item.name != "Backstage passes to a TAFKAL80ETC concert"
+            ):
+                if item.quality > 0:
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -31,15 +41,11 @@ class GildedRose(object):
                                 item.quality = item.quality + 1
 
             if item.name != "Sulfuras, Hand of Ragnaros":
-
                 item.sell_in = item.sell_in - 1
 
             if item.sell_in < 0:
-
                 if item.name != "Aged Brie":
-
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
-
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
                                 item.quality = item.quality - 1
@@ -47,7 +53,5 @@ class GildedRose(object):
                         item.quality = item.quality - item.quality
                 else:
 
-
                     if item.quality < 50:
                         item.quality = item.quality + 1
-
--- gilded_rose/test_gilded_rose.py
+++ gilded_rose/test_gilded_rose.py
@@ -1,6 +1,9 @@
-from gilded_rose.main import Item, GildedRose
+from gilded_rose.main import GildedRose, Item
+
+
 def test_foo():
+    """basic test"""
     items = [Item("foo", 0, 0)]
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
-    assert items[0].name == "fixme"
+    assert items[0].name == "foo"
